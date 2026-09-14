4077901aee61b123680b11ce4ce460559b97da58
Bahar Pala <109612572+mcd2023@users.noreply.github.com>
2026-09-14 14:29:36 +0300

    Delete main.js

--- main.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{const b=document.querySelector(".menu"),n=document.querySelector(".nav nav");if(b&&n)b.addEventListener("click",()=>n.classList.toggle("open"));});
